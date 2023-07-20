9ad536ad23c6624c9645717b28d83e2dcba6ba7a
vinnyWebDev <vinnywebdev@gmail.com>
2023-07-20 21:37:12 +0100

    filter working

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
         <Route path="/about" element={<About/>}/>
         <Route path="/getcrypto" element={<GetCrypto/>}/>
       </Routes>
-
+    
     </div>
   );
 }
--- src/components/GetCrypto.js
+++ src/components/GetCrypto.js
@@ -2,13 +2,18 @@ import React, { useEffect } from "react";
 import { useState } from "react";
 import axios from 'axios';
 import ShowCrypto from "./ShowCrypto";
+import { InputGroup } from "react-bootstrap";
+import { Form } from "react-bootstrap";
 
 function GetCrypto() {
+
 
     //Store retreived data here
     const [coinData, setCoinData] = useState([]);
     //Pull coin data from this url
     const apiUrl = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false&locale=en";
+    //used for filtering 
+    const [query, setQuery] = useState("");
 
     //Pulls coin data from our API
     useEffect(() => {
@@ -22,8 +27,17 @@ function GetCrypto() {
 
     return (
         <div className="container">
+            {/*}This is a form which uses bootstrap. Purpose is to filter the many coins we have pulled{*/}
+            <form>
+                <InputGroup>
+                    <Form.Control onChange={(e) => setQuery(e.target.value)} placeholder="Filter crypto"></Form.Control>
+                </InputGroup>
+            </form>
             {
-                coinData.map(function (i, index) {
+                //use filter method with query to narrow down list of coins
+                coinData.filter((i)=>{
+                    return query.toLowerCase() === "" ? i : i.id.toLowerCase().includes(query)
+                }).map(function (i, index) {
                     return (
                         <ShowCrypto image={i.image} name={i.name} price={i.current_price} dailyPercentage={i.price_change_percentage_24h} marketCap = {i.market_cap_rank} key={i.id} />
                     )
